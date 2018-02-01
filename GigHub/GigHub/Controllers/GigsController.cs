012d5a856b1eecb20d2212f9d01f209f36af1056
Asif Rahman <asif0531@live.com>
2018-02-01 21:07:13 +0600

    validation implemented for gig

--- GigHub/GigHub/Controllers/GigsController.cs
+++ GigHub/GigHub/Controllers/GigsController.cs
@@ -30,11 +30,16 @@ namespace GigHub.Controllers
         [HttpPost]
         public ActionResult Create(GigFormViewModel viewModel)
         {
+            if (!ModelState.IsValid)
+            {
+                viewModel.Genres = _context.Genres.ToList();
+                return View(viewModel);
+            }
             var gigModel = new Gig()
             {
                 Venue = viewModel.Venue, 
                 ArtistId = User.Identity.GetUserId() , 
-                DateTime = viewModel.DateTime, 
+                DateTime = viewModel.GetDateTime(), 
                 GenreId = viewModel.Genre 
             };
             _context.Gigs.Add(gigModel);
--- GigHub/GigHub/ViewModels/GigFormViewModel.cs
+++ GigHub/GigHub/ViewModels/GigFormViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 using GigHub.Models;
@@ -8,19 +9,23 @@ namespace GigHub.ViewModels
 {
     public class GigFormViewModel
     {
-        public DateTime DateTime
-        {
-            get
-            {
-                return DateTime.Parse($"{Date} {Time}");
-            }
-        }
-
+        
+        [Required]
         public string Venue { get; set; }
+        [Required]
+        [FutureDate]
         public string Date { get; set; }
+        [Required]
+        [ValidTime]
         public string Time { get; set; }
+        [Required]
         public byte Genre { get; set; }
         public IEnumerable<Genre> Genres { get; set; }
-        
+
+        public DateTime GetDateTime()
+        {
+            return DateTime.Parse($"{Date} {Time}");
+        }
+
     }
 }
